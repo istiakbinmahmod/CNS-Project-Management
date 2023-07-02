367d4751a9cf729227ca6a08972d22d48870d69a
masumk086@gmail.com <yournewemail@example.com>
2023-07-02 15:00:45 +0600

    first commit

---
+++ src/main/java/com/cns/cns/project/management/CnsProjectManagementApplication.java
@@ -0,0 +1,13 @@
+package com.cns.cns.project.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CnsProjectManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CnsProjectManagementApplication.class, args);
+	}
+
+}
